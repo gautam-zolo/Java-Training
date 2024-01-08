704ca5c4bd29fbfc51ba42a7c5aceab96d99bff3
Gautam <gautam.si@zolostays.com>
2024-01-08 11:16:36 +0530

    Created my first Thread program

---
+++ FirstRunnable/src/gautam/runnable/FirstRunnable.java
@@ -0,0 +1,13 @@
+package gautam.runnable;
+import java.util.*;
+import java.lang.*;
+public class FirstRunnable {
+    public static void main(String[] args) {
+        Runnable runnable = () ->{
+            System.out.print("I am running in "+Thread.currentThread().getName());
+        };
+        Thread t = new Thread(runnable);
+        t.setName("My thread");
+        t.start();
+    }
+}
